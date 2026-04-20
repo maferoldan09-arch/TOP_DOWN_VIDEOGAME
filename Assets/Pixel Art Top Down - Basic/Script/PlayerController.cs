00c2d11c35e49e26e7ff2c7e789f3861592bff37
maferoldan09-arch <mafe.roldan.09@gmail.com>
2026-04-19 23:15:01 -0500

    Coleccionables y rotación

--- Assets/Pixel Art Top Down - Basic/Script/PlayerController.cs
+++ Assets/Pixel Art Top Down - Basic/Script/PlayerController.cs
@@ -7,16 +7,16 @@ public class PlayerController : MonoBehaviour
     public int score = 0;
     public bool hasKey = false;
     public bool hasWater = false;
-    public TextMeshProUGUI textScore; //variable para conectar con nuestro Texto en la UI
-     public TextMeshProUGUI notificationText;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public TextMeshProUGUI textScore; 
+    public TextMeshProUGUI notificationText;
+
     void Start()
     {
-       UpdateTextScore(); 
+       // ❌ COMENTAR porque textScore es null
+       // UpdateTextScore(); 
     }
 
-    // Update is called once per frame
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -26,26 +26,29 @@ public class PlayerController : MonoBehaviour
         transform.Translate(direction * speed * Time.deltaTime);
     }
 
-    //este método especial de unity se ejecuta cuando interactuamos con un objeto en modo Trigger
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Collectable"))
         {
-            score = score + 1;//le sumo 1 a la variable score
-            UpdateTextScore();
-            ShowNotification("Collected!");; 
+            score = score + 1;
+
+            // ❌ COMENTAR
+            // UpdateTextScore();
+            // ShowNotification("Collected!"); 
 
             Destroy(other.gameObject);
             Debug.Log("Collected!");
             Debug.Log("Score: " + score);
-           
         }
+
         if(other.CompareTag("Key"))
         {
             hasKey = true;
             score = score + 5;
-            UpdateTextScore();
-            ShowNotification("KEY Collected!"); 
+
+            // ❌ COMENTAR
+            // UpdateTextScore();
+            // ShowNotification("KEY Collected!"); 
 
             Debug.Log("KEY Collected!");
             Destroy(other.gameObject);
@@ -54,30 +57,31 @@ public class PlayerController : MonoBehaviour
         if(other.CompareTag("Water"))
         {
             hasWater = true;
-            ShowNotification("Has tocado el agua, no puedes ganar");
+
+            // ❌ COMENTAR
+            // ShowNotification("Has tocado el agua, no puedes ganar");
+
             Debug.Log("Has tocado el agua, comunicate con el servicio tecnico de Samsung, Oh!");
-            
         }
                 
-        if (score >= 3 && hasKey == true && !hasWater) //
+        if (score >= 3 && hasKey == true && !hasWater)
         {
-            ShowNotification("You Won!");
+            // ❌ COMENTAR
+            // ShowNotification("You Won!");
+
             Debug.Log("You Won!");
         }
-        /*else
-        {
-             Debug.Log("Keep trying, you need 3 and the KEY to win");
-        }*/
     }
 
     void UpdateTextScore()
     {
-         textScore.text = "Score: " + score;
+        // ❌ COMENTAR
+        // textScore.text = "Score: " + score;
     }
 
     void ShowNotification(string message)
     {
-         notificationText.text = message; 
+        // ❌ COMENTAR
+        // notificationText.text = message; 
     }
-
 }
