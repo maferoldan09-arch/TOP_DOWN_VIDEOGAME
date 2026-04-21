6e86b4b8f1dc595a56368c80164d24a553048e8c
maferoldan09-arch <mafe.roldan.09@gmail.com>
2026-04-20 21:24:22 -0500

    Contadores

--- Assets/Pixel Art Top Down - Basic/Script/PlayerController.cs
+++ Assets/Pixel Art Top Down - Basic/Script/PlayerController.cs
@@ -4,17 +4,31 @@ using TMPro;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+
+    // PUNTAJE TOTAL
     public int score = 0;
 
-    public bool hasCofre = false;  
-    public bool hasTumba = false;   
+    // CONTADORES
+    public int monedas = 0;
+    public int totalMonedas = 60;
 
-    public TextMeshProUGUI textScore; 
+    public int cofres = 0;
+    public int totalCofres = 2;
+
+    public bool hasCofre = false;
+    public bool hasTumba = false;
+
+    // EXTOS UI
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI monedasText;
+    public TextMeshProUGUI cofresText;
     public TextMeshProUGUI notificationText;
 
     void Start()
     {
-       UpdateTextScore(); 
+        UpdateScore();
+        UpdateMonedas();
+        UpdateCofres();
     }
 
     void Update()
@@ -28,54 +42,78 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        // MONEDAS
         if(other.CompareTag("Collectable"))
         {
-            score = score + 1;
+            monedas++;
+            score += 1;
 
-            UpdateTextScore();
-            ShowNotification("Collected!"); 
+            UpdateMonedas();
+            UpdateScore();
+
+            ShowNotification("Flor recogida!");
 
             Destroy(other.gameObject);
-            Debug.Log("Collected!");
-            Debug.Log("Score: " + score);
+            Debug.Log("Flor recogida");
         }
 
+        // COFRE
         if(other.CompareTag("Cofre"))
         {
+            cofres++;
             hasCofre = true;
-            score = score + 5;
+            score += 5;
 
-            UpdateTextScore();
-            ShowNotification("Encontraste el cofre!"); 
+            UpdateCofres();
+            UpdateScore();
 
-            Debug.Log("Encontraste el cofre!");
+            ShowNotification("Cofre recogido!");
+
             Destroy(other.gameObject);
+            Debug.Log("Cofre recogido");
         }
 
+        // TUMBA
         if(other.CompareTag("Tumba"))
         {
             hasTumba = true;
 
-            ShowNotification("Has tocado la runa, no puedes ganar");
+            ShowNotification("Has tocado una tumba");
 
-            Debug.Log("Has tocado la runa, no puedes ganar");
+            Debug.Log("Has tocado la tumba");
         }
-                
-        if (score >= 3 && hasCofre == true && !hasTumba)
-        {
-          ShowNotification("Ganaste!");
 
+        // CONDICION DE VICTORIA
+        if (monedas == totalMonedas && cofres == totalCofres && !hasTumba)
+        {
+            ShowNotification("¡Ganaste!");
             Debug.Log("Ganaste!");
         }
     }
 
-    void UpdateTextScore()
+    // ACTUALIZAR UI
+
+    void UpdateScore()
     {
-        textScore.text = "Score: " + score;
+        if(scoreText != null)
+            scoreText.text = "Puntaje: " + score;
+    }
+
+    void UpdateMonedas()
+    {
+        if(monedasText != null)
+            monedasText.text = "Monedas: " + monedas + "/" + totalMonedas;
+    }
+
+    void UpdateCofres()
+    {
+        if(cofresText != null)
+            cofresText.text = "Cofres: " + cofres + "/" + totalCofres;
     }
 
     void ShowNotification(string message)
     {
-     notificationText.text = message; 
+        if(notificationText != null)
+            notificationText.text = message; 
     }
 }
